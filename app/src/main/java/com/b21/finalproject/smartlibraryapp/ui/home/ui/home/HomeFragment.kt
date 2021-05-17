ddba59a47a74cf0729e54da6a690c238e59f8dde
ramdhanjr11 <ramdhanjr690@gmail.com>
2021-05-17 13:05:11 +0700

    added more item in HomeFragment.kt for ui on xml

--- app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/home/HomeFragment.kt
@@ -1,13 +1,13 @@
 package com.b21.finalproject.smartlibraryapp.ui.home.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.b21.finalproject.smartlibraryapp.R
 import com.b21.finalproject.smartlibraryapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,22 +19,35 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val reqActivity = requireActivity() as AppCompatActivity
+        reqActivity.setSupportActionBar(binding.layoutHeaderHome.homeToolbar)
+        reqActivity.setTitle("")
+
+        val textView: TextView = binding.layoutHeaderHome.tvUsername
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.actionbar_home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onDestroyView() {
