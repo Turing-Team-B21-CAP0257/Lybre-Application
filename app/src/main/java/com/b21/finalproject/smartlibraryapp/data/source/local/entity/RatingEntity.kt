38600564aeae04414cfab3989ec0c292c6487578
ramdhanjr11 <ramdhanjr690@gmail.com>
2021-05-28 11:12:41 +0700

    Add DetailBookActivity.kt and fix layout for detail book

--- app/src/main/java/com/b21/finalproject/smartlibraryapp/data/source/local/entity/RatingEntity.kt
+++ app/src/main/java/com/b21/finalproject/smartlibraryapp/data/source/local/entity/RatingEntity.kt
@@ -1,10 +1,13 @@
 package com.b21.finalproject.smartlibraryapp.data.source.local.entity
 
+import android.os.Parcelable
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "rating_tb")
 data class RatingEntity(
     @PrimaryKey()
@@ -17,4 +20,4 @@ data class RatingEntity(
 
     @ColumnInfo(name = "book_rating")
     var book_rating: String
-)
+) : Parcelable
---
+++ app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/detail/DetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.b21.finalproject.smartlibraryapp.ui.home.ui.detail
+
+import androidx.lifecycle.ViewModel
+import com.b21.finalproject.smartlibraryapp.data.source.BookRepository
+
+class DetailViewModel(private val bookRepository: BookRepository) : ViewModel() {
+
+
+
+}
