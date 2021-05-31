a836bb4b8f7b9fd02b57baea64a65ca6a49933f5
yossyhds <yossyhds>
2021-05-31 08:55:20 +0700

    added multi language for indonesia and english, added more in strings.xml

--- app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/settings/SettingsActivity.kt
@@ -1,13 +1,31 @@
 package com.b21.finalproject.smartlibraryapp.ui.home.ui.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import com.b21.finalproject.smartlibraryapp.R
+import com.b21.finalproject.smartlibraryapp.databinding.ActivitySettingsBinding
+import com.b21.finalproject.smartlibraryapp.ui.auth.LoginFragment
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var settingsBinding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        settingsBinding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(settingsBinding.root)
 
+        settingsBinding.layoutLogout.tvLogoutTitle.setOnClickListener {
+            val intent = Intent(this@SettingsActivity, LoginFragment::class.java)
+            startActivity(intent)
+        }
+
+        settingsBinding.layoutHeaderChangeLanguage.tvChangeLanguageTitle.setOnClickListener {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
     }
+
 }
