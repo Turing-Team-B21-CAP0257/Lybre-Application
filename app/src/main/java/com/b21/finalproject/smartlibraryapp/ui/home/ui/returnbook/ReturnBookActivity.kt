bfd2646a4d009fa8405bcc49993619f7f353ce62
ramdhanjr11 <ramdhanjr690@gmail.com>
2021-06-04 22:38:14 +0700

    added function to get current location and permission for location access

--- app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/returnbook/ReturnBookActivity.kt
+++ app/src/main/java/com/b21/finalproject/smartlibraryapp/ui/home/ui/returnbook/ReturnBookActivity.kt
@@ -1,10 +1,13 @@
 package com.b21.finalproject.smartlibraryapp.ui.home.ui.returnbook
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.b21.finalproject.smartlibraryapp.data.source.local.entity.BookWithDeadlineEntity
@@ -13,6 +16,8 @@ import com.b21.finalproject.smartlibraryapp.prefs.AppPreference
 import com.b21.finalproject.smartlibraryapp.ui.home.HomeActivity
 import com.b21.finalproject.smartlibraryapp.utils.SortUtils
 import com.b21.finalproject.smartlibraryapp.viewModel.ViewModelFactory
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class ReturnBookActivity : AppCompatActivity() {
 
@@ -21,7 +26,14 @@ class ReturnBookActivity : AppCompatActivity() {
     private lateinit var viewModel: ReturnBookViewModel
     private lateinit var appPreference: AppPreference
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     private lateinit var binding: ActivityReturnBookBinding
+
+    companion object {
+        private val GPS_REQUEST = 100
+        private val GPS_RESULT = 101
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +65,10 @@ class ReturnBookActivity : AppCompatActivity() {
             }
         })
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        checkLocationPermission()
+
         adapter.setOnBtnReturnClickCallback(object : ReturnBookAdapter.OnBtnReturnClickCallback {
             override fun onBtnItemClickCallback(book: BookWithDeadlineEntity) {
                 viewModel.updateBorrowBook(1, book.bookId)
@@ -61,5 +77,21 @@ class ReturnBookActivity : AppCompatActivity() {
                 finish()
             }
         })
+    }
+
+    private fun checkLocationPermission() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), GPS_REQUEST)
+            return
+        }
+        task.addOnSuccessListener {
+            if (it != null) {
+                Log.d("Location", " ${it.latitude} ${it.longitude}")
+            }
+        }
     }
 }
